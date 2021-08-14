39ea179493dfa17a2d05e56f5c7316dae3ed4839
Nabetani <takenori@nabetani.sakura.ne.jp>
2021-08-14 13:48:21 +0900

    ファイルを読むようにする

--- kahancha.cpp
+++ kahancha.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <tuple>
+#include <cstdio>
+#include <filesystem>
+#include <fstream>
 
 float sum(float const *begin, float const *end)
 {
@@ -54,25 +58,50 @@ float sum_of_parts(float const *begin, float const *end)
 
 using sumproc_t = float(float const *begin, float const *end);
 
-void show( char const * name, sumproc_t * proc, float const * b, float const * e, float expected)
+void show(char const *name, sumproc_t *proc, float const *b, float const *e, float expected)
 {
-    float actual = proc(b,e);
-    std::printf( "%15s %16.10e %9.3e\n", name, actual, actual-expected);
+    float actual = proc(b, e);
+    std::printf("%15s %16.10e %9.3e\n", name, actual, actual - expected);
+}
+
+void test(float expected, std::vector<float> const &data)
+{
+    auto b = data.data();
+    auto e = b + data.size();
+    show("sum", sum, b, e, expected);
+    show("with_double", with_double, b, e, expected);
+    show("sum_of_parts", sum_of_parts, b, e, expected);
+    show("kahan", kahan, b, e, expected);
+}
+
+std::tuple<std::string, float, std::vector<float>>
+read_file(char const *fn)
+{
+    auto size = std::filesystem::file_size(fn);
+    auto count = size / sizeof(float);
+    auto p = std::make_unique<float[]>(count);
+    auto fs = std::ifstream(fn);
+    fs.read(reinterpret_cast<char *>(p.get()), size);
+    auto e = p[0];
+    std::vector<float> data(p.get() + 1, p.get() + count);
+    return {fn, e, data};
+}
+
+std::tuple<std::string, float, std::vector<float>>
+create_data()
+{
+    std::vector<float> ones(1 << 25);
+    for (auto &e : ones)
+    {
+        e = 1.0f;
+    }
+    return {"2**25 ones", 1 << 25, ones};
 }
 
 int main(int argc, char const *argv[])
 {
-    std::vector<float> d(1<<25);
-    for (size_t i = 0; i < d.size(); ++i)
-    {
-        d[i] = 1;
-    }
-    auto b = d.data();
-    auto e = b + d.size();
-    auto expected = with_double(b,e);
-    show( "sum", sum, b, e ,expected);
-    show( "with_double", with_double, b, e ,expected);
-    show( "sum_of_parts", sum_of_parts, b, e ,expected);
-    show( "kahan", kahan, b, e ,expected);
+    auto [title, expected, data] = argc == 2 ? read_file(argv[1]) : create_data();
+    std::cout << title << std::endl;
+    test(expected, data);
     return 0;
 }
